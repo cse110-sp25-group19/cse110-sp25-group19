9a02d8508f05dc1c3532e66274d1cb2993c6d0c3
Menko <menko@hornstein.me>
2025-06-04 11:25:13 -0700

    refactor: update module exports to ES6 syntax and remove base href from index.html

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,12 @@
-const { Card, generateDeck, shuffleDeck, GameState } = require('../js/utils');
+import {
+  Card,
+  generateDeck,
+  shuffleDeck,
+  GameState,
+  addScore,
+  getScore,
+  matchCheck,
+} from './utils.js';
 /**
  * Initializes a new game round.
  *
@@ -297,7 +305,7 @@ function handleTimeOut() {
 // Export this so it can be used when cards match
 //export { updateScore };
 
-module.exports = {
+export {
   initGame,
   renderBoard,
   showEndScreen,
